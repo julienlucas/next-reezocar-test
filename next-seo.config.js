d308753724ea1184ae4b70e1a10bbc98f013cc1b
Julien Lucas <contact@julienlucas.com>
2021-04-15 23:12:41 +0200

    Initial commit

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,7 +3,28 @@ const description = "Concessionnaire en ligne spécialiste de l'achat et la vent
 const SEO = {
   title: `${description} | Meilleure reprise`,
   description,
-  canonical: 'https://meilleure-reprise.com'
+  canonical: 'https://meilleure-reprise.com',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://meilleure-reprise.com',
+    title: 'Meilleure-reprise.com',
+    description,
+    images: [
+      {
+        url: '/images/header-home.png',
+        width: 900,
+        height: 800,
+        alt: 'Og Image Alt Second',
+      }
+    ],
+    site_name: 'Meilleure-reprise.com',
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  }
 }
 
 export default SEO
